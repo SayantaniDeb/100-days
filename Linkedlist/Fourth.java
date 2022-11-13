119ef7a87c4c57d227e3a55d568707ef08e0af97
Sayantani Deb <sayantani.1001@gmail.com>
2022-11-14 00:23:05 +0530

    second commit

--- Linkedlist/Fourth.java
+++ Linkedlist/Fourth.java
@@ -1,4 +1,33 @@
 package Linkedlist;
 public class Fourth {
+    //Linkedlist Cycle
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            ListNode fast=head;
+            ListNode slow=head;
     
+            while(fast!=null&&fast.next!=null){
+                fast=fast.next.next;
+                slow=slow.next;
+                if(fast==slow){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    //Get intersection node of linkedlist
+    public class Solution2 {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            ListNode p1=headA;
+            ListNode p2=headB;
+            while(p1!=p2){
+                p1=p1==null?headB:p1.next;
+                p2=p2==null?headA:p2.next;
+        }
+        return p1;
+        
+    }
+    }
+
 }
