212efd01aacb4320999f2a1ea24fcd99e1a89fcd
Sayantani Deb <sayantani.1001@gmail.com>
2022-11-15 22:24:28 +0530

    Day 3

---
+++ Linkedlist/Day 3/Fifth.java
@@ -0,0 +1,48 @@
+package Linkedlist.Day 3;
+
+public class Fifth {
+    class Solution {
+        public void reorderList(ListNode head) {
+            if(head==null || head.next==null){
+                return;
+            }
+            ListNode mid=middle(head);
+            ListNode hs=reverse(mid);
+            ListNode hf=head;
+    
+            while(hf!=null && hs!=null){
+                ListNode temp=hf.next;
+                hf.next=hs;
+                hf=temp;
+                temp=hs.next;
+                hs.next=hf;
+                hs=temp;
+            }
+            if(hf!=null){
+                hf.next=null;
+            }
+        }
+        public ListNode middle(ListNode head){
+            ListNode slow=head;
+            ListNode fast=head;
+    
+            while(fast!=null && fast.next!=null){
+                slow=slow.next;
+                fast=fast.next.next;
+    
+            }
+            return slow;
+        }
+        public ListNode reverse(ListNode head){
+            if(head==null || head.next==null){
+                return head;
+            }
+            ListNode newHead= reverse(head.next);
+            ListNode headNext=head.next;
+            headNext.next=head;
+            head.next=null;
+            return newHead;
+        }
+    }
+    
+}
